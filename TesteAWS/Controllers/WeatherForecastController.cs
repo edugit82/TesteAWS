23345a4051d9134008df16b5624569937a5f0f8a
Eduardo correa dos Santos <eduardocorrea82@hotmail.com>
2025-08-22 13:51:10 -0300

    22-08-205 13:50

--- TesteAWS/Controllers/WeatherForecastController.cs
+++ TesteAWS/Controllers/WeatherForecastController.cs
@@ -9,7 +9,7 @@ namespace TesteAWS.Controllers
         private static readonly string[] Summaries = new[]
         {
             //"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering" 
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot" 
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
