def62c24ff0fa7f36854f3e43c0727662103b5db
Eduardo correa dos Santos <eduardocorrea82@hotmail.com>
2025-08-22 15:03:07 -0300

    22-08-2025 15:02

--- TesteAWS/Controllers/WeatherForecastController.cs
+++ TesteAWS/Controllers/WeatherForecastController.cs
@@ -9,7 +9,7 @@ namespace TesteAWS.Controllers
         private static readonly string[] Summaries = new[]
         {
             //"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
@@ -20,15 +20,24 @@ namespace TesteAWS.Controllers
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        public string Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            string retorno = "";
+
+            WeatherForecast[] temperaturas = Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
+
+            temperaturas.ToList().ForEach(x =>
+            {
+                retorno += $"{x.Date} - {x.TemperatureC}°C - {x.Summary}\n";
+            });
+
+            return retorno;            
         }
     }
 }
